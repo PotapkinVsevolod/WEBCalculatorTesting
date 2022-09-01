b7e1041b199612ecda8aeedaf119c7ee55626f6c
PotapkinVsevolod <potapkinvsevolod@gmail.com>
2022-09-01 11:31:16 +0300

    Добавил тесты для целочисленным делением и делением с остатком.

--- Tests/test_multiplication.py
+++ Tests/test_multiplication.py
@@ -2,6 +2,8 @@ import os
 import requests
 import json
 
+API_URL = "http://localhost:17678/api/multiplication"
+
 def setup_module(module):
     os.system('C:\\Python\\infotecs_test_task\\webcalculator.exe start')
     
@@ -11,45 +13,50 @@ def teardown_module(module):
 
 def test_max():
     test_dict = {"x": 2147483647, "y": 2147483647}
-    response = json.loads(requests.post('http://localhost:17678/api/multiplication', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 4611686014132420609
 
 def test_min():
     test_dict = {"x": -2147483648, "y": -2147483648}
-    response = json.loads(requests.post('http://localhost:17678/api/multiplication', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 4611686018427387904
 
 def test_max_min():
     test_dict = {"x": 2147483647, "y": -2147483648 }
-    response = json.loads(requests.post('http://localhost:17678/api/multiplication', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == -4611686016279904256
 
 def test_min_max():
     test_dict = {"x": -2147483648, "y": 2147483647 }
-    response = json.loads(requests.post('http://localhost:17678/api/multiplication', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == -4611686016279904256
 
 def test_max_zero():
     test_dict = {"x": 2147483647, "y": 0}
-    response = json.loads(requests.post('http://localhost:17678/api/multiplication', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 0
 
 def test_min_zero():
     test_dict = {"x": -2147483648, "y": 0}
-    response = json.loads(requests.post('http://localhost:17678/api/multiplication', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 0
 
 def test_zero_max():
     test_dict = {"x": 0, "y": 2147483647}
-    response = json.loads(requests.post('http://localhost:17678/api/multiplication', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 0
 
 def test_zero_min():
     test_dict = {"x": 0, "y": -2147483648}
-    response = json.loads(requests.post('http://localhost:17678/api/addition', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 0
     
 def test_zero_zero():
     test_dict = {"x": 0, "y": 0}
-    response = json.loads(requests.post('http://localhost:17678/api/addition', json=test_dict).text)
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 0
+
+def test_max_minus_one():
+    test_dict = {"x": 2147483647, "y": -1}
+    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
+    assert response["result"] == -2147483647
--- Tests/test_remainder.py
+++ Tests/test_remainder.py
@@ -2,7 +2,7 @@ import os
 import requests
 import json
 
-API_URL = "http://localhost:17678/api/addition"
+API_URL = "http://localhost:17678/api/remainder"
 
 def setup_module(module):
     os.system('C:\\Python\\infotecs_test_task\\webcalculator.exe start')
@@ -14,12 +14,12 @@ def teardown_module(module):
 def test_max():
     test_dict = {"x": 2147483647, "y": 2147483647}
     response = json.loads(requests.post(url=API_URL, json=test_dict).text)
-    assert response["result"] == 4294967294
+    assert response["result"] == 0
 
 def test_min():
     test_dict = {"x": -2147483648, "y": -2147483648}
     response = json.loads(requests.post(url=API_URL, json=test_dict).text)
-    assert response["result"] == -4294967296
+    assert response["result"] == 0
 
 def test_max_min():
     test_dict = {"x": 2147483647, "y": -2147483648 }
@@ -29,29 +29,19 @@ def test_max_min():
 def test_min_max():
     test_dict = {"x": -2147483648, "y": 2147483647 }
     response = json.loads(requests.post(url=API_URL, json=test_dict).text)
-    assert response["result"] == -1
-
-def test_max_zero():
-    test_dict = {"x": 2147483647, "y": 0}
-    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
-    assert response["result"] == 2147483647
-
-def test_min_zero():
-    test_dict = {"x": -2147483648, "y": 0}
-    response = json.loads(requests.post(url=API_URL, json=test_dict).text)
-    assert response["result"] == -2147483648
+    assert response["result"] == 2147483646
 
 def test_zero_max():
     test_dict = {"x": 0, "y": 2147483647}
     response = json.loads(requests.post(url=API_URL, json=test_dict).text)
-    assert response["result"] == 2147483647
+    assert response["result"] == 0
 
 def test_zero_min():
     test_dict = {"x": 0, "y": -2147483648}
     response = json.loads(requests.post(url=API_URL, json=test_dict).text)
-    assert response["result"] == -2147483648
-    
-def test_zero_zero():
-    test_dict = {"x": 0, "y": 0}
+    assert response["result"] == 0
+
+def test_max_minus_one():
+    test_dict = {"x": 2147483647, "y": -1}
     response = json.loads(requests.post(url=API_URL, json=test_dict).text)
     assert response["result"] == 0
